821bec07c3e125b3ae29d1a76735121fd0c787e9
Giovanna Albuquerque <ghb.albuquerque@gmail.com>
2021-02-04 17:25:06 -0300

    pasta pages/ pasta components/ pasta assets/img

--- src/app/grupo1/grupo1.module.ts
+++ src/app/grupo1/grupo1.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TesteComponent } from './teste/teste.component';
+import { PagesComponent } from './pages/pages.component';
+import { ComponentsComponent } from './components/components.component';
 
 
 @NgModule({
-  declarations: [TesteComponent],
+  declarations: [TesteComponent, PagesComponent, ComponentsComponent],
   imports: [
     CommonModule
   ]
