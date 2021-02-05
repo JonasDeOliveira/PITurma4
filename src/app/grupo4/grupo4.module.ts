4b078dc2ce79a9390d83acf1caf9dc9c503b9550
gloriiakang <lori.vihchoi@gmail.com>
2021-02-05 02:28:55 -0300

    Migração Tela Agenda Médico

--- src/app/grupo4/grupo4.module.ts
+++ src/app/grupo4/grupo4.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CadastroMedicoComponent } from './cadastro-medico/cadastro-medico.component';
 import { HistoricoPacienteComponent } from './historico-paciente/historico-paciente.component';
+import { AgendaMedicoComponent } from './agenda-medico/agenda-medico.component';
 
 
 @NgModule({
-  declarations: [CadastroMedicoComponent, HistoricoPacienteComponent],
+  declarations: [CadastroMedicoComponent, HistoricoPacienteComponent, AgendaMedicoComponent],
   imports: [
     CommonModule
   ]
