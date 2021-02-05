c4fae5fb3d3aefc05ef03768bc6228ec88fedf98
Steicy <steicykelly@yahoo.com.br>
2021-02-05 14:56:10 -0300

    método voltar

--- src/app/grupo4/historico-paciente/historico-paciente.component.ts
+++ src/app/grupo4/historico-paciente/historico-paciente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-historico-paciente',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoricoPacienteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _location: Location) { }
 
   ngOnInit(): void {
+  }
+
+  voltar() {
+    this._location.back();
   }
 
 }
