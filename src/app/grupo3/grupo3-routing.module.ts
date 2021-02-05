99f2928e0c27f538a0de421ce9dda3173e82be77
Denize A <denizealmeidas@gmail.com>
2021-02-05 14:35:15 -0300

    agservico, historico-agservico, procurar-paciente

--- src/app/grupo3/grupo3-routing.module.ts
+++ src/app/grupo3/grupo3-routing.module.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
+import { AgservicoComponent } from './agservico/agservico.component';
 import { HistoricoAgservicoComponent } from './historico-agservico/historico-agservico.component';
+import { ProcurarPacienteComponent } from './procurar-paciente/procurar-paciente.component';
 
 
 export const Grupo3RoutingModule: Routes = [
-    {path:'historico-agservico', component:HistoricoAgservicoComponent}
+    {path:'historico-agservico', component:HistoricoAgservicoComponent},
+    {path:'agservico', component:AgservicoComponent},
+    {path:'procurar-paciente', component:ProcurarPacienteComponent}
+
 ];
 
--- src/app/grupo3/grupo3.module.ts
+++ src/app/grupo3/grupo3.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HistoricoAgservicoComponent } from './historico-agservico/historico-agservico.component';
 import { AgservicoComponent } from './agservico/agservico.component';
+import { ProcurarPacienteComponent } from './procurar-paciente/procurar-paciente.component';
 
 
 @NgModule({
-  declarations: [HistoricoAgservicoComponent, AgservicoComponent],
+  declarations: [HistoricoAgservicoComponent, AgservicoComponent, ProcurarPacienteComponent],
   imports: [
     CommonModule
   ]
