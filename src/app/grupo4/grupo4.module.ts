a358b8c3d311053434cfedd6b81c7c4e5b144f13
Carol <carolbenetao@gmail.com>
2021-02-05 14:18:07 -0300

    Rotas de links

--- src/app/grupo4/grupo4.module.ts
+++ src/app/grupo4/grupo4.module.ts
@@ -1,25 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import { PerfilMedicoComponent } from './perfil-medico/perfil-medico.component';
-import { DashboardMedicoComponent } from './dashboard-medico/dashboard-medico.component';
+import { RouterModule } from '@angular/router';
 
-
-
-@NgModule({
-  declarations: [PerfilMedicoComponent, DashboardMedicoComponent],
-=======
 import { CadastroMedicoComponent } from './cadastro-medico/cadastro-medico.component';
 import { HistoricoPacienteComponent } from './historico-paciente/historico-paciente.component';
 import { AgendaMedicoComponent } from './agenda-medico/agenda-medico.component';
 import { LoginMedicoComponent } from './login-medico/login-medico.component';
 import { SolicitacaoExamesComponent } from './solicitacao-exames/solicitacao-exames.component';
+import { PerfilMedicoComponent } from './perfil-medico/perfil-medico.component';
+import { DashboardMedicoComponent } from './dashboard-medico/dashboard-medico.component';
 
 @NgModule({
-  declarations: [CadastroMedicoComponent, HistoricoPacienteComponent, AgendaMedicoComponent, LoginMedicoComponent, SolicitacaoExamesComponent],
->>>>>>> 166fe4417cc40b07af9fe337b944450aab15b37b
+  declarations: [CadastroMedicoComponent, HistoricoPacienteComponent, AgendaMedicoComponent, LoginMedicoComponent, SolicitacaoExamesComponent, PerfilMedicoComponent, DashboardMedicoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class Grupo4Module { }
