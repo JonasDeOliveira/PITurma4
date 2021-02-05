97f15d3990dab163a61c7680d7f67851bd1f8a77
grazielle-perillo <grazy.perillo@hotmail.com>
2021-02-05 12:45:40 -0300

    rotas cadastro e historico

--- src/app/grupo4/grupo4-routing.module.ts
+++ src/app/grupo4/grupo4-routing.module.ts
@@ -1,15 +1,5 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-import { DashboardMedicoComponent } from './dashboard-medico/dashboard-medico.component';
-import { PerfilMedicoComponent } from './perfil-medico/perfil-medico.component';
 
-
-export const Grupo4RoutingModule: Routes = [
-
-    {path: 'perfil/medico' , component: PerfilMedicoComponent},
-    {path: 'dashboard/medico', component: DashboardMedicoComponent}
-=======
-import { from } from 'rxjs';
 import { AtendimentoEspecialistaComponent } from './atendimento-especialista/atendimento-especialista.component';
 import { CadastroMedicoComponent } from './cadastro-medico/cadastro-medico.component';
 import { HistoricoPacienteComponent } from './historico-paciente/historico-paciente.component';
@@ -17,7 +7,8 @@ import { PrescreverMedicacaoComponent } from './prescrever-medicacao/prescrever-
 import { AgendaMedicoComponent } from './agenda-medico/agenda-medico.component';
 import { LoginMedicoComponent } from './login-medico/login-medico.component';
 import { SolicitacaoExamesComponent } from './solicitacao-exames/solicitacao-exames.component';
-
+import { DashboardMedicoComponent } from './dashboard-medico/dashboard-medico.component';
+import { PerfilMedicoComponent } from './perfil-medico/perfil-medico.component';
 
 export const Grupo4RoutingModule: Routes = [
     {path: 'cadastro/medico' , component: CadastroMedicoComponent},
@@ -26,7 +17,7 @@ export const Grupo4RoutingModule: Routes = [
     { path: 'prescrever/medicacao', component: PrescreverMedicacaoComponent},
     { path: 'agenda/medico', component: AgendaMedicoComponent},
     { path: 'login/medico', component:LoginMedicoComponent},
-    { path: 'solicitacao/exames', component:SolicitacaoExamesComponent}
->>>>>>> 166fe4417cc40b07af9fe337b944450aab15b37b
-  
+    { path: 'solicitacao/exames', component:SolicitacaoExamesComponent},
+    {path: 'perfil/medico' , component: PerfilMedicoComponent},
+    {path: 'dashboard/medico', component: DashboardMedicoComponent}
 ];
--- src/app/grupo4/grupo4.module.ts
+++ src/app/grupo4/grupo4.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
+import { RouterModule } from '@angular/router';
 import { PerfilMedicoComponent } from './perfil-medico/perfil-medico.component';
 import { DashboardMedicoComponent } from './dashboard-medico/dashboard-medico.component';
-
-
-
-@NgModule({
-  declarations: [PerfilMedicoComponent, DashboardMedicoComponent],
-=======
 import { CadastroMedicoComponent } from './cadastro-medico/cadastro-medico.component';
 import { HistoricoPacienteComponent } from './historico-paciente/historico-paciente.component';
 import { AgendaMedicoComponent } from './agenda-medico/agenda-medico.component';
@@ -16,10 +10,11 @@ import { LoginMedicoComponent } from './login-medico/login-medico.component';
 import { SolicitacaoExamesComponent } from './solicitacao-exames/solicitacao-exames.component';
 
 @NgModule({
-  declarations: [CadastroMedicoComponent, HistoricoPacienteComponent, AgendaMedicoComponent, LoginMedicoComponent, SolicitacaoExamesComponent],
->>>>>>> 166fe4417cc40b07af9fe337b944450aab15b37b
+  declarations: [PerfilMedicoComponent,DashboardMedicoComponent,CadastroMedicoComponent, HistoricoPacienteComponent, AgendaMedicoComponent, LoginMedicoComponent, SolicitacaoExamesComponent],
+
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class Grupo4Module { }
