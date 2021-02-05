f5655b823fb7ded41e85cc77833cf66f065b0622
Camila Pacheco <camilapach@gmail.com>
2021-02-05 12:04:25 -0300

    buscar receitas e resultado buscar receitas

--- src/app/grupo3/grupo3-routing.module.ts
+++ src/app/grupo3/grupo3-routing.module.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
-
+import { PesquisarReceitasComponent } from './pesquisar-receitas/pesquisar-receitas.component'
+import { ResultadoBuscarReceitasComponent } from './resultado-buscar-receitas/resultado-buscar-receitas.component';
 
 export const Grupo3RoutingModule: Routes = [
-
+    {path: 'pesquisar-receitas', component: PesquisarReceitasComponent},
+    {path: 'resultado-buscar-receitas', component: ResultadoBuscarReceitasComponent}
   
 ];
+
--- src/app/grupo3/grupo3.module.ts
+++ src/app/grupo3/grupo3.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { PesquisarReceitasComponent } from './pesquisar-receitas/pesquisar-receitas.component';
+import { ResultadoBuscarReceitasComponent } from './resultado-buscar-receitas/resultado-buscar-receitas.component';
+import {RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [],
+  declarations: [PesquisarReceitasComponent, ResultadoBuscarReceitasComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class Grupo3Module { }
