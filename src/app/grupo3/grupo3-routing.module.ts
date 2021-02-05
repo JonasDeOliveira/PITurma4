00adbb67f204a3f7a5e3c91d93f7db2f7ac5a5ba
Denize A <denizealmeidas@gmail.com>
2021-02-05 11:56:45 -0300

    historico ag servico

--- src/app/grupo3/grupo3-routing.module.ts
+++ src/app/grupo3/grupo3-routing.module.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
+import { HistoricoAgservicoComponent } from './historico-agservico/historico-agservico.component';
 
 
 export const Grupo3RoutingModule: Routes = [
-
-  
+    {path:'historico-agservico', component:HistoricoAgservicoComponent}
 ];
+
--- src/app/grupo3/grupo3.module.ts
+++ src/app/grupo3/grupo3.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HistoricoAgservicoComponent } from './historico-agservico/historico-agservico.component';
+import { AgservicoComponent } from './agservico/agservico.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HistoricoAgservicoComponent, AgservicoComponent],
   imports: [
     CommonModule
   ]
