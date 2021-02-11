b20a303374401db779ff89ae2db241d6d6e19686
Carol <carolbenetao@gmail.com>
2021-02-11 14:55:27 -0300

    alterações Perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { Grupo1Module } from './grupo1/grupo1.module';
 import { Grupo2Module } from './grupo2/grupo2.module';
 import { Grupo3Module } from './grupo3/grupo3.module';
 import { Grupo4Module } from './grupo4/grupo4.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,7 @@ import { AppComponent } from './app.component';
     Grupo1Module,
     Grupo2Module,
     Grupo3Module,
-    Grupo4Module
+    Grupo4Module,
   ],
   providers: [],
   bootstrap: [AppComponent]
