8a10b0f16af855dc8def5455c61a8c3a6caa5313
gloriiakang <lori.vihchoi@gmail.com>
2021-02-11 22:32:16 -0300

    Add modal abrir agenda e cancelar consulta + ajustes nos horários do dashboard e agenda

--- src/app/grupo4/agenda-medico/agenda-medico.component.ts
+++ src/app/grupo4/agenda-medico/agenda-medico.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-agenda-medico',
   templateUrl: './agenda-medico.component.html',
-  styleUrls: ['./agenda-medico.component.css']
+  styleUrls: ['./agenda-medico.component.css'],
+  providers: [NgbModalConfig, NgbModal]
 })
 export class AgendaMedicoComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+   }
+
+   open(content) {
+     this.modalService.open(content);
+   }
 
   ngOnInit(): void {
   }
