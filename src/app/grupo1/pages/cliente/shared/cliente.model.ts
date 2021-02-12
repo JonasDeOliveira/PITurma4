e8e3b3dcd9f9c6700ec94a6cb44d1deb0119a259
Monique Kimie Hamatsu <monique_hamatsu@hotmail.com>
2021-02-12 19:14:42 -0300

    Correção Cliente

--- src/app/grupo1/pages/cliente/shared/cliente.model.ts
+++ src/app/grupo1/pages/cliente/shared/cliente.model.ts
@@ -2,24 +2,35 @@ import { Lembrete } from 'src/app/grupo1/lembretes/shared/lembrete.model'
 import { Planos } from '../../planos/shared/planos.model';
 import { Usuario } from '../../../usuario/shared/usuario.model';
 
-export interface OutputCliente {
-   
-        loginUsuario: LoginUsuario,
-        usuario:Usuario,
-        ddd: string,
-        celular: string,
-        contrato: Contrato,
-        //cartao: Cartao 
-        nmNome: string,
-        idPaciente: number,
-        lembretes:Array <Lembrete> 
-        uf:  Array<Uf>,
-        genero: Array<Genero> 
-        planos: Array<Planos> 
-        dsEmail: string,
-        contatos: Array<Contato>;
-        ufs: Array<Uf>,
-        idPlano: number
+    export interface InputCliente {
+    loginUsuario: LoginUsuario,
+    usuario: Usuario,
+    ddd: String,
+    celular: String,
+    contrato: Contrato,
+    //cartao: Cartao    
+    }
+    //----------------------------------------------------
+    export interface AreaDoCliente{
+    nmNome: String,
+    idPaciente: number,
+    lembrete: Array<Lembrete> 
+    }
+    //----------------------------------------------------
+    export interface FormularioCadastro{
+    ufs: Array<Uf>,
+    genero: Array<Genero>,
+    planos: Array<Planos>
+    }
+     //----------------------------------------------------
+    export interface FormularioMeusDados{
+    usuario: Usuario,
+    dsEmail: string,
+    contatos: Array<Contato>,
+    ufs: Array<Uf>,
+    genero: Array<Genero>,
+    planos: Array<Planos>,
+    idPlano: number
     }
     //----------------------------------------------------
     export interface Contrato {
