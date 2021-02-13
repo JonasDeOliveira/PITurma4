b9864a374391f2671ed5c96b06435c06f66b96ad
ThayanaAraujo <tharaujo@rd.com.br>
2021-02-13 01:00:35 -0300

    correção erro minhas-consultas

--- src/app/grupo2/pages/pagamento-plano/pagamento-plano.component.ts
+++ src/app/grupo2/pages/pagamento-plano/pagamento-plano.component.ts
@@ -27,6 +27,7 @@ export class PagamentoPlanoComponent implements OnInit {
       }
     )
 
+
     }
   }
 
--- src/app/grupo2/shared/services/pagamento-plano-service.service.ts
+++ src/app/grupo2/shared/services/pagamento-plano-service.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,8 @@ import { Injectable } from '@angular/core';
 })
 export class PagamentoPlanoServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private readonly APICADASTRARPAGTOPLANO = 'http://localhost:8080/pagamento/plano';
+  
 }
