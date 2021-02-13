13327d57dcb6de2dca95d379bedff30b1eb46196
jessica <jessica.c.m.cunha@hotmail.com>
2021-02-13 17:31:02 -0300

    alteração no app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { Grupo3Module } from './grupo3/grupo3.module';
 import { Grupo4Module } from './grupo4/grupo4.module';
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [
     AppComponent
