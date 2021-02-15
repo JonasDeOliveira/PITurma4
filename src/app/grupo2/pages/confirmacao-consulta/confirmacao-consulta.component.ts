5e803a6432323dd52a57db968de3a023fda7b5ce
Maina <fisiomainalex@gmail.com>
2021-02-15 17:20:01 -0300

    integracao das APIs da tela de confirmacao com angular

--- src/app/grupo2/pages/confirmacao-consulta/confirmacao-consulta.component.ts
+++ src/app/grupo2/pages/confirmacao-consulta/confirmacao-consulta.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ResponseAgPaciente } from '../../shared/model/agPaciente';
+import { ResponsePagamento } from '../../shared/model/pagamento';
+import { ResponseTipoConsulta } from '../../shared/model/tipoConsulta';
+import { AgPacienteService } from '../../shared/services/agPaciente.service';
+import { PagamentoService } from '../../shared/services/pagamento.service';
+import { TipoConsultaService } from '../../shared/services/tipo-consulta.service';
 
 @Component({
   selector: 'app-confirmacao-consulta',
@@ -9,17 +15,49 @@ import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ConfirmacaoConsultaComponent implements OnInit {
 
-  constructor(config: NgbModalConfig, private modalService: NgbModal) {
-    // customize default values of modals used by this component tree
+  constructor(
+    config: NgbModalConfig, private modalService: NgbModal,
+    public pagamentoService: PagamentoService,
+    public agPacienteService: AgPacienteService,    
+    public tipoConsultaService: TipoConsultaService
+    
+    ) {
+    
     config.backdrop = 'static';
     config.keyboard = false;
+
   }
 
+  reponsePagamento: ResponsePagamento;
+  responseTipoConsulta: ResponseTipoConsulta;
+  responseAgPaciente: ResponseAgPaciente;
+
   ngOnInit(): void {
+    this.listarPagamento(33);
+    this.listarAgPaciente(3);
+    this.listarTipoConsulta(1)
   }
 
   open(content) {
     this.modalService.open(content);
   }
 
+  listarPagamento(idPagamento: number){
+    this.pagamentoService.buscarPagamento(idPagamento).subscribe(
+      response => {this.reponsePagamento = response}
+    )
+  };
+  listarAgPaciente(idAgPaciente: number){
+    this.agPacienteService.listarAgPacientePorId(idAgPaciente).subscribe(
+      response => {this.responseAgPaciente = response}
+    )
+  };
+  listarTipoConsulta(idTipoConsulta: number){
+    this.tipoConsultaService.buscarTipoConsulta(idTipoConsulta).subscribe(
+      response => {this.responseTipoConsulta = response}
+    )
+  };
+
 }
+
+
