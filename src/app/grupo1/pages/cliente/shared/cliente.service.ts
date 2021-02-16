bf47635aaf7b074282365d20a585fdb4ce5afbc9
Jemima <jemima.ramoscampanha@gmail.com>
2021-02-16 14:02:12 -0300

    Card Plano - Area Cliente

--- src/app/grupo1/pages/cliente/shared/cliente.service.ts
+++ src/app/grupo1/pages/cliente/shared/cliente.service.ts
@@ -1,11 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-<<<<<<< HEAD
 import { Injectable } from '@angular/core';
-import { ResponseCidades, ResponseFormularioCadastro, ResponseFormularioMeusDados, ResponseAreadoCliente} from './cliente.model';
-=======
-import { Injectable, Output } from '@angular/core';
-import { OutputCliente, ResponseCidades, ResponseFormularioCadastro, ResponseFormularioMeusDados} from './cliente.model';
->>>>>>> 8b2d62cc2e62841f1bf68c70080c9cd5bfa37ce2
+import { ResponseCidades, ResponseFormularioCadastro, ResponseFormularioMeusDados, ResponseAreadoCliente,OutputCliente} from './cliente.model';
 import { Observable } from 'rxjs';
 
 
@@ -15,7 +10,6 @@ import { Observable } from 'rxjs';
   export class ClienteService {
    
     constructor(private http: HttpClient) { }
-<<<<<<< HEAD
   
     private readonly API = 'http://localhost:8080';
      
@@ -39,32 +33,8 @@ import { Observable } from 'rxjs';
       return this.http.get<ResponseAreadoCliente>(URL);
 
   }
-  
-  getPlanoAtualById(idUsuario:number): Observable<ResponseAreadoCliente>{
-  const URL = `${this.API}/cliente/area-cliente/${idUsuario}`
-      return this.http.get<ResponseAreadoCliente>(URL);
-  }
-
-}
-=======
-  
-    private readonly API = 'http://localhost:8080';
-         
-    getFormularioCadastro(){
-       const URL = `${this.API}/cliente/cadastro`
-      return this.http.get<ResponseFormularioCadastro>(URL);
-    }
-
-    getFormularioMeusDados(idUsuario: number): Observable<ResponseFormularioMeusDados>{
-        const URL = `${this.API}/cliente/meus-dados/${idUsuario}`
-       return this.http.get<ResponseFormularioMeusDados>(URL);
-    }
-    getCidadesByUf(idUf: number): Observable<ResponseCidades>{
-        const URL = `${this.API}/cidadeBuscar/${idUf}`
-        return this.http.get<ResponseCidades>(URL);
-    }
+ 
     createCliente(outputCliente: OutputCliente): Observable<OutputCliente> {
         return this.http.post<OutputCliente>('http://localhost:8080/cliente/cadastrar', outputCliente);
     }
     }
->>>>>>> 8b2d62cc2e62841f1bf68c70080c9cd5bfa37ce2
